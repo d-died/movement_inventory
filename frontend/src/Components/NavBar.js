ba8218e18b07502d1ddc157ca128efb634ae78fd
timrusin <timrusin@gmail.com>
2022-09-05 18:33:30 -0500

    basic navbar styling for desktop not mobile responsive yet

--- frontend/src/Components/NavBar.js
+++ frontend/src/Components/NavBar.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './Navbar.css'
 
 
 const NavBar = () => {
 
     return (
         <div className="navbar">
-             
-            <Link to="/" className="navitem">BTS</Link>
-            <Link to="/ManagerForm" className="navitem">Arist Manager Form</Link>
-            <Link to="/BacklineLibrary" className="navitem">Backline Library</Link>
-            <Link to="/StageMgrProfiles" className="navitem">Stage Manager Profiles</Link>
+             <div id="links-container">
+                <Link to="/" className="navitem">BTS</Link>
+                <Link to="/ManagerForm" className="navitem">Arist Manager Form</Link>
+                <Link to="/BacklineLibrary" className="navitem">Backline Library</Link>
+                <Link to="/StageMgrProfiles" className="navitem">Stage Manager Profiles</Link>
+            </div>
             
         </div>
     )
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import * as ReactDOMCLient from 'react-dom/client'
 import App from "./App"
 import { BrowserRouter as Router } from 'react-router-dom'
+import './index.css'
 
 
 ReactDOMCLient.createRoot(document.querySelector('#root')).render(
