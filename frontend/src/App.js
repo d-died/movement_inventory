001af7bdc15d82be00cb56e5b33e63078a799e0e
timrusin <timrusin@gmail.com>
2022-09-16 15:08:16 -0500

    added home screen and route please excuse my humor :)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,14 +8,15 @@ import NavBar from "./Components/NavBar";
 import StageManagerProfiles from "./Components/StageMgmtProfiles";
 import BacklineLibrary from "./Components/BacklineLibrary";
 import ArtistManagerForm from "./Components/ArtistManagerForm";
+import BTSHome from "./Components/BTSHome";
 
 const App = () => {
-
 
     return (
         <div>
             <NavBar />
             <Routes>
+                <Route path="/" element={<BTSHome />}/>
                 <Route path="/ManagerForm" element={ <ArtistManagerForm /> }/>
                 <Route path="/BacklineLibrary" element={ <BacklineLibrary /> }/>
                 <Route path="/StageMgrProfiles" element={ <StageManagerProfiles />}/>
---
+++ frontend/src/Components/BTSHome.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './BTSHome.css'
+
+const BTSHome = () => {
+  return (
+    <div className='home-page-container'>
+        <h1>HOME SCREEN</h1>
+      <img
+        src="https://images.indianexpress.com/2022/06/BTS1200-1.jpeg"
+        alt="bts"
+        width="80%"
+      />
+    </div>
+  );
+}
+
+export default BTSHome
